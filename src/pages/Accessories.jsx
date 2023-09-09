b5531bd5a7aa87a906d2a2a0abd8fb68d52dde89
Hodjaevv <hodjaevabu@gmail.com>
2023-09-09 19:18:39 +0500

    rasm o'zgardi

--- src/pages/Accessories.jsx
+++ src/pages/Accessories.jsx
@@ -2,19 +2,21 @@ import Category from "../components/Category";
 import Footer from "../components/Footer";
 import Sort from "../components/Sort";
 
-import photo4 from "../assets/images/Crossbody-Bag.png"
-import Diorbag from "../assets/images/Diorbag.png"
-import photo10 from "../assets/images/photo-10.svg"
+import photo4 from "../assets/images/Crossbody-Bag.png";
+import Diorbag from "../assets/images/Diorbag.png";
+import photo10 from "../assets/images/photo-10.svg";
 
 const Accessories = () => {
   return (
-    <div className="container mx-auto pt-[100px] px-12 w-[1400px]">
-      <div className="flex">
-        <Category title="ACCESSORIES" />
-        <Sort image={photo4} image2={Diorbag} image3={photo10} />
+    <div>
+      <div className="container mx-auto pt-[100px] px-12 w-[1400px]">
+        <div className="flex">
+          <Category title="ACCESSORIES" />
+          <Sort image={photo4} image2={Diorbag} image3={photo10} />
+        </div>
       </div>
-      <Footer/>
-      </div>
+      <Footer />
+    </div>
   );
 };
 
